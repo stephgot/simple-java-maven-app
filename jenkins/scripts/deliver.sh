c40720becc2eb77c1f2f3b8f11cbcdc42179e289
sga <sga@wanadoo.fr>
2024-11-29 10:24:49 +0100

    debug3

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -27,4 +27,4 @@ echo $NAME
 echo $VERSION
 #ls -l target/${NAME}-${VERSION}.jar
 #java -jar target/${NAME}-${VERSION}.jar
-java -tar target/my-app-1.0-SNAPSHOT.jar
+java -jar target/my-app-1.0-SNAPSHOT.jar
